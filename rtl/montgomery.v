b568529f1ea2deeaa06a936116d57d0552fea6ba
YZ <charliehe0211@gmail.com>
2021-11-07 21:29:15 +0100

    multiplication loop

--- rtl/montgomery.v
+++ rtl/montgomery.v
@@ -1,33 +1,127 @@
 `timescale 1ns / 1ps
 
-module montgomery(
-    input           clk,
-    input           resetn,
-    input           start,
-    input  [1023:0] in_a,
-    input  [1023:0] in_b,
-    input  [1023:0] in_m,
-    output [1023:0] result,  
-    output          done
-     );
- 
+module montgomery(input clk,
+                  input resetn,
+                  input start,
+                  input [1023:0] in_a,
+                  input [1023:0] in_b,
+                  input [1023:0] in_m,
+                  output [1023:0] result,
+                  output done);
+//                  output a_i,
+//                  output div_clk,
+//                  output done_b,
+//                  output done_m,
+//                  output [1027:0] adder_b_out,
+//                  output [1027:0] adder_m_out,
+//                  output [1026:0] C_out);
+    
     /*
-    Student tasks:
-    1. Instantiate an Adder
-    2. Use the Adder to implement the Montgomery multiplier in hardware.
-    3. Use tb_montgomery.v to simulate your design.
-    */
+     Student tasks:
+     1. Instantiate an Adder
+     2. Use the Adder to implement the Montgomery multiplier in hardware.
+     3. Use tb_montgomery.v to simulate your design.
+     */
     
-     
+    // clock divider
+    reg clk_div5;
+    reg [3:0] counter;
+    parameter clk_div_period = 5;
     
+    always @(posedge clk) begin
+        if (~resetn | counter >= clk_div_period-1)
+            counter <= 4'b0;
+        else
+        begin
+            counter  <= counter + 1;
+            clk_div5 <= (~counter[0] & ~counter[1] & ~counter[2] & ~counter[3]);
+        end
+    end
+    
+    // shift register for input A
+    // shift input A every 5 clock cycles
+    reg [1023:0] in_a_reg;
+    always @(posedge clk_div5)
+    begin
+        if (~resetn)
+            in_a_reg <= 1023'b0;
+        else if (start)
+            in_a_reg <= in_a;
+        else
+            in_a_reg <= in_a_reg >> 1;
+    end
+    
+    // define wires for connecting C and adders
+    wire [1027:0] regC_D;
+    reg [1027:0] regC_Q;
+    wire [1026:0] shifted_C;
+    
+    // instantiate Adder B
+    wire adder_b_done;
+    wire [1027:0] adder_b_result;
+    mpadder adder_B (
+    .clk      (clk),
+    .resetn   (resetn),
+    .start    (clk_div5),
+    .subtract (1'b0),
+    .in_a     (shifted_C),
+    .in_b     ({3'b0, in_b}),
+    .result   (adder_b_result),
+    .done     (adder_b_done));
+    
+    // define the multiplexer for adder B result
+    wire [1027:0] muxOutAdder_B;
+    assign muxOutAdder_B = (in_a_reg[0] == 1) ? adder_b_result : shifted_C;
+    
+    // instantiate Adder M
+    wire adder_m_done;
+    wire [1027:0] adder_m_result;
+    mpadder adder_M (
+    .clk      (clk),
+    .resetn   (resetn),
+    .start    (adder_b_done),
+    .subtract (1'b0),
+    .in_a     (muxOutAdder_B),
+    .in_b     ({3'b0, in_m}),
+    .result   (adder_m_result),
+    .done     (adder_m_done));
+    
+    // define the multiplexer for adder M result
+    wire [1027:0] muxOutAdder_M;
+    assign muxOutAdder_M = (muxOutAdder_B[0] == 1) ? adder_m_result : muxOutAdder_B;
+    // connect regC_D to muxOutAdder_M
+    assign regC_D = muxOutAdder_M;
+    
+    // define regC
+    always @(posedge clk)
+    begin
+    if (~resetn || start)
+        regC_Q <= 1028'b0;
+    else if (adder_m_done)
+        regC_Q <= regC_D;
+    end
+    
+    // define shifted C
+    assign shifted_C = regC_Q >> 1'b1;
+    
+    assign result = shifted_C;
+    
+//    assign a_i = in_a_reg[0];
+//    assign div_clk = clk_div5;
+//    assign done_b      = adder_b_done;
+//    assign done_m = adder_m_done;
+//    assign adder_b_out = adder_b_result;
+//    assign adder_m_out = adder_m_result;
+//    assign C_out       = shifted_C;
+
     //This always block was added to ensure the tool doesn't trim away the montgomery module.
     //Students: Feel free to remove this block
-    reg [1023:0] r_result;
-    always @(posedge(clk))
-    begin
-        r_result <= {1024{1'b1}};
-    end
-    assign result = r_result;
-
-    assign done = 1;
+//            reg [1023:0] r_result;
+//            always @(posedge(clk))
+//            begin
+//                r_result <= {1024{1'b1}};
+//            end
+//            assign result = r_result;
+    
+            assign done = 1;
 endmodule
--- rtl_tb/tb_montgomery.v
+++ rtl_tb/tb_montgomery.v
@@ -14,56 +14,71 @@ module tb_montgomery();
     reg  [1023:0] in_m;
     wire [1023:0] result;
     wire         done;
-
+    
     reg  [1023:0] expected;
     reg          result_ok;
+
+//    wire a_i;
+//    wire div_clk;
+//    wire done_b;
+//    wire done_m;
+//    wire [1027:0] b_out;
+//    wire [1027:0] m_out;
+//    wire [1026:0] c_out;
     
     //Instantiating montgomery module
-    montgomery montgomery_instance( .clk    (clk    ),
-                                    .resetn (resetn ),
-                                    .start  (start  ),
-                                    .in_a   (in_a   ),
-                                    .in_b   (in_b   ),
-                                    .in_m   (in_m   ),
-                                    .result (result ),
-                                    .done   (done   ));
-
+    montgomery montgomery_instance(.clk    (clk),
+    .resetn (resetn),
+    .start  (start),
+    .in_a   (in_a),
+    .in_b   (in_b),
+    .in_m   (in_m),
+    .result (result),
+    .done   (done));
+//    .a_i  (a_i),
+//    .div_clk (div_clk),
+//    .done_b  (done_b),
+//    .done_m (done_m),
+//    .adder_b_out (b_out),
+//    .adder_m_out (m_out),
+//    .C_out (c_out));
+   
     //Generate a clock
     initial begin
-        clk = 0;
+        clk                    = 0;
         forever #`CLK_HALF clk = ~clk;
     end
     
     //Reset
     initial begin
-        resetn = 0;
+        resetn              = 0;
         #`RESET_TIME resetn = 1;
     end
     
     // Test data
     initial begin
-
+        
         #`RESET_TIME
         
         // You can generate your own with test vector generator python script
-        in_a     <= 1024'h993a45a7ccc9834b9775fbbf1be8566199cf3883f29a2846a7357f314b1b6a719edf9b543addb902a023885c72b83d21d6bc2a14ed8adef3566f0da4c541f83c882bc4a1c21cb045d17eb1f773535af04b82a90c5823ac4f3076dbbd37c7019cb84e7b2e21849abd2860ef93d144c8564492ec8036b3d1a51bd98c94c145d8c3;
-        in_b     <= 1024'ha21716d123007a82e337ffa9de869fbebb20cd18955897dc2b9e096146b56d5706ee40c1d38b6ab293f1ea65d8d8c98bbaad3ddd301080f3f1e11e02aa5c19b22a62d225bab93a13b629bfb493792fed1b7fa42442888c8e71b6de10dedeaa77a5c8a15bad9380346279ebc55720df62186e222a698a238f472e7d24ad2c6b2f;
-        in_m     <= 1024'hc195d759bc8a96f3a59c363f4f4d8b596ce12e0aca41fca232eb07944fc92b2f798a902095c69a1c8c3ecd049169fe7ed2d1ae296658546e5b500edcf935e9a1a11ab841332a7a430f188012e42162703fbfba6be85ada06ec9a4fd80dab436c7a60ef5c1aa0a67c4542049b94dc649e57c56f782498df91c78f7c463cb6329d;
-        expected <= 1024'h2029a1aea1321437516fe9dd75d3d95eed38ea38b190c4e4d566d103d4b0831b28a6fc19127e7d92c8913a4b53c8470aadf95fa738eb8908590223f58c0f63939e33bf4eab1f29b7a7742509fa0bc7814f11cd28a1ff05666e47d1c9a32462744257ba69c6165bb158792c4959bc3b874c89b6985a95a13512b98bd72c9db18e;
+        in_a     <= 1024'h9da047286698f2932ccdcde2caf34892b5083b6ef547230d1a6bed0da3fc78d72077d83a31a9424acfe163b207771176b6c65dd7a805034a6b0116c56eebfbc8b8c19bfcade49f58569b76406a21f3178d13fa5d89c1fbe1ea3385eb3865e4cfa25c0672597aee9573e8f2dda745e2b3a5d473bcc8b1b3834b9d13403137b47e;
+        in_b     <= 1024'h325fbd7c1f8b70dc75209a0b742349cb71020cbbae206caed0e657e384d7b16bcfd33d90bf6368ee24f8737298ec20303c05601234f4741fe1aaf4120128ef7350bdcaaaa3ef118fe1572a3d40de00bdf0198db7abc69dcec9ebbfed1c4787ca567cbb40f0f682ea5f4b7f31df94a2a9eb5741e69ef568e23451578e0424f325;
+        in_m     <= 1024'hb8a242847799283ecc87a7d432450e4bc686e7f8947a639ee22d97bb63b70870a9d3e62374d5a22a0284140b57cde7db72878f2fff7fb27b2f754a9c1cb1e9826c26628929281df434e4819a0580b6dc6241ac8cd47cf83b2ec6a906f5a50a896161f4ddce9287409809153ea430f3440baaefdb5987d0301b6ce6832ec34c47;
+        expected <= 1024'h281039c3ddbd9e2d18f42b34a85628f26727b07c8ef17fa3b62f287676351d095789018b901c666ea2412a0e944bd1b15186981d2f7f4e18886e9d1d9a288ff1c269f68f4377f5631ad8f73e11aadf1d55ea9a6a85cb0bc918931b5abde557cb6e72d8703cd60f292a050eba94b62d5967ff57639a5b9dde742ea0df6661aa60;
         
-        start<=1;
+        start <= 1;
         #`CLK_PERIOD;
-        start<=0;
+        start <= 0;
         
-        wait (done==1);
+        wait (done == 1);
         
-        $display("result calculated=%x", result);
-        $display("result expected  =%x", expected);
-        $display("error            =%x", expected-result);
-        result_ok = (expected==result);
-        #`CLK_PERIOD;   
+        $display("result calculated = %x", result);
+        $display("result expected   = %x", expected);
+        $display("error             = %x", expected-result);
+        result_ok                   = (expected == result);
+        #`CLK_PERIOD;
         
         $finish;
     end
-           
+    
 endmodule
